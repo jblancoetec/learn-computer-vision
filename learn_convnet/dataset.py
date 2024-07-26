56db629f246c7bf41d7a811155cf07cd72d2dc8e
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-07-26 00:13:53 -0300

    se expone un archivo .env.example como referencia para la configuración de las variables de entorno

--- learn_convnet/dataset.py
+++ learn_convnet/dataset.py
@@ -11,18 +11,24 @@ app = ty.Typer()
 @app.command()
 def main():
     try:
-        download_dataset_if_does_not_exist(RAW_DATA_DIR)
+        download_dataset(RAW_DATA_DIR)
     except Exception as e:
         log.error(f"Error downloading dataset: {e}")
 
 
-def download_dataset_if_does_not_exist(raw_data_dir: pl.Path):
+def download_dataset(raw_data_dir: pl.Path):
     log.info("Downloading data...")
     raw_data_dir.resolve().mkdir(parents=True, exist_ok=True)
     kg.api.dataset_download_files(
         dataset="ryanholbrook/car-or-truck", path=raw_data_dir, unzip=True
     )
     log.success("Data downloaded.")
+
+
+def process_dataset(raw_data_dir: pl.Path, processed_data_dir: pl.Path):
+    raw_data_train_dir = raw_data_dir / "train"
+    raw_data_valid_dir = raw_data_dir / "valid"
+    pass
 
 
 if __name__ == "__main__":
